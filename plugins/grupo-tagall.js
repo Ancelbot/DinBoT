ea0bfad4f839ad8952b8f10ee5f91c0570c56cfe
Ancelbot <135066230+JossXvB@users.noreply.github.com>
2024-02-06 16:12:11 -0500

    🦖

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -1,21 +1,21 @@
-let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}
-let vn = './media/Invocar.mp3'
-let pesan = args.join` `
-let oi = `*𝙈𝙚𝙣𝙨𝙖𝙟𝙚:* ${pesan}`
-let teks = `╭┄〔 *${wm}* 〕┄⊱\n┆ 𝘼𝙘𝙩𝙞𝙫𝙚𝙣𝙨𝙚 𝙥𝙡𝙖𝙣𝙩𝙖𝙨 🙊*\n┆\n┆❏ ${oi}\n┆\n┆❏ *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
-for (let mem of participants) {
-teks += `┆🙈 @${mem.id.split('@')[0]}\n`}
-teks += `└ @𝐦𝐨𝐧𝐤𝐢_𝐛𝐨𝐭 🐵 \n\n`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
-conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
-}
-handler.help = ['tagall <mesaje>','invocar <mesaje>']
-handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
-handler.admin = true
-handler.group = true
-export default handler
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+    if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    if (!(isAdmin || isOwner)) {
+      global.dfail('admin', m, conn);
+      throw false;
+    }
+    const pesan = args.join` `;
+    const oi = `*@ancelzzz:* ${pesan}`;
+    let teks = `*⺀𝑳𝑶𝑺 𝑰𝑵𝑽𝑶𝑪𝑶𝑶𝑶 𝑮𝑨𝑨𝑨𝑨🏴‍☠️⺀*\n\n ${oi}\n\n Iᥒ𝗍ᥱgrᥲᥒ𝗍ᥱs:🦖\n`;
+    for (const mem of participants) {
+      teks += `*・🦖 @${mem.id.split('@')[0]}\n`;
+    }
+    teks += `*└* 𝕯𝖎𝖓𝖔𝕭𝖔𝖙 🦖\n\n*🏴‍☠️*`;
+    conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+  };
+  handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
+  handler.tags = ['group'];
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+  handler.admin = true;
+  handler.group = true;
+  export default handler;
--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -21,23 +21,10 @@ import fetch from 'node-fetch';
      const taguser = '@' + m.sender.split('@s.whatsapp.net')[0]; 
      const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document']; 
      const document = doc[Math.floor(Math.random() * doc.length)]; 
-     const str = `𝐂𝐑𝐄𝐀𝐃𝐎𝐑𝐄𝐒 𝐃𝐄 𝐒𝐇𝐀𝐍𝐀 𝐁𝐎𝐓 ♓
+     const str = `𝐂𝐑𝐄𝐀𝐃𝐎𝐑 𝕯𝖎𝖓𝖔𝕭𝖔𝖙 🦖 ♓
 
-• 𝐂𝐫𝐞𝐚𝐝𝐨𝐫 𝐨𝐟𝐜: 𝗗𝗮𝗻𝗶𝗲𝗹 🇦🇱
-
-𝐈𝐠: Instagram.com/Jxtxn17/
-𝐍𝐮𝐦𝐞𝐫𝐨: Wa.me/51907913096
-
-
-• 𝐒𝐨𝐩𝐨𝐫𝐭𝐞 𝐨𝐟𝐜: 𝗦𝗼𝗽𝗶𝗶 🧸
-
-𝐈𝐠: Instagram.com/sophziix/
-𝐍𝐮𝐦𝐞𝐫𝐨: Wa.me/51977835959
-
-• 𝐂𝐨𝐥𝐚𝐛𝐨𝐫𝐚𝐝𝐨𝐫 𝐨𝐟𝐜: 𝗔𝘅 ⚜
-
-𝐈𝐠: Instagram.com/Litopeax
-𝐍𝐮𝐦𝐞𝐫𝐨: Wa.me/51929972576
+• 𝐂𝐫𝐞𝐚𝐝𝐨𝐫 𝐨𝐟𝐜: JossX
+𝐍𝐮𝐦𝐞𝐫𝐨: Wa.me/56986469687
   `.trim(); 
      if (m.isGroup) { 
        // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true}) 
